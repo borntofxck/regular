0f2b97656f3cf2253da4cd54a35cd8494b7a5a2f
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-10-04 22:48:43 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+
+            string reg = @"^(^[;:])(-*)(\(+|\)+|\[+|\]+)$";
+
+
+            if (Regex.IsMatch(str1, reg))
+            {
+                Console.WriteLine($"СМАЙЛИКИ: {str1}");
+            }
+
+            else
+            {
+                Console.WriteLine($"НЕ ЯВЛЯЮТСЯ СМАЙЛИКАМИ: {str1}");
+            }
+            
+        }
+    }
+}
